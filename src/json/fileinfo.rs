5325cb051564c675696edd8acfcf5fe651d0459c
irevoire <irevoire@protonmail.ch>
2021-02-27 18:39:08 +0100

    implement a first iteration of the listing without the sorting thing

--- src/json/fileinfo.rs
+++ src/json/fileinfo.rs
@@ -8,25 +8,25 @@ use std::path::Path;
 #[derive(Debug, Default, Serialize)]
 pub struct FileInfo {
     #[serde(flatten)]
-    listing: Option<Listing>,
+    pub(super) listing: Option<Listing>,
 
-    path: String,
-    name: String,
-    size: u64,
-    extension: String,
+    pub(super) path: String,
+    pub(super) name: String,
+    pub(super) size: u64,
+    pub(super) extension: String,
     #[serde(rename = "modified")]
-    mod_time: String,
-    mode: u32,
+    pub(super) mod_time: String,
+    pub(super) mode: u32,
     #[serde(rename = "isDir")]
-    is_dir: bool,
+    pub(super) is_dir: bool,
     #[serde(rename = "type")]
-    file_type: String,
+    pub(super) file_type: String,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    subtitles: Vec<String>,
+    pub(super) subtitles: Vec<String>,
     #[serde(skip_serializing_if = "String::is_empty")]
-    content: String,
+    pub(super) content: String,
     #[serde(skip_serializing_if = "HashMap::is_empty")]
-    checksums: HashMap<String, String>,
+    pub(super) checksums: HashMap<String, String>,
 }
 
 impl FileInfo {
@@ -40,31 +40,46 @@ impl FileInfo {
         Ok(base)
     }
 
-    pub fn build(&mut self, root: &Path, route: &Path) -> Result<()> {
+    pub(super) fn from_path_without_listing(root: &Path, route: &Path) -> Result<Self> {
+        let mut base = Self::default();
+        base.build_without_listing(root, route)?;
+        Ok(base)
+    }
+
+    pub(super) fn build_without_listing(&mut self, root: &Path, route: &Path) -> Result<()> {
         let path = root.join(route);
         let metadata = path.metadata()?;
 
+        self.is_dir = path.is_dir();
+
+        if self.is_dir {
+            self.name = path.file_name().unwrap().to_str().unwrap().to_string();
+        } else {
+            self.name = path.file_stem().unwrap().to_str().unwrap().to_string();
+            self.file_type = mime_guess::from_ext(&self.extension)
+                .first()
+                .map(|guess| guess.type_().as_str().to_string())
+                .unwrap_or("".to_string());
+            self.extension = path
+                .extension()
+                .map(|path| path.to_str().unwrap().to_string())
+                .unwrap_or("".to_string());
+        }
+
         self.path = route.to_str().unwrap().to_string();
-        self.name = path
-            .file_stem()
-            .unwrap_or_else(|| path.file_name().unwrap())
-            .to_str()
-            .unwrap()
-            .to_string();
         self.size = metadata.len();
-        self.extension = path
-            .extension()
-            .map(|path| path.to_str().unwrap().to_string())
-            .unwrap_or("".to_string());
         let time: chrono::DateTime<chrono::Utc> = metadata.modified().unwrap().into();
         self.mod_time = time.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
         self.mode = metadata.permissions().mode();
-        self.is_dir = path.is_dir();
-        self.file_type = mime_guess::from_ext(&self.extension)
-            .first()
-            .map(|guess| guess.type_().as_str().to_string())
-            .unwrap_or("".to_string());
 
+        Ok(())
+    }
+
+    pub fn build(&mut self, root: &Path, route: &Path) -> Result<()> {
+        self.build_without_listing(root, route)?;
+        if self.is_dir {
+            self.listing = Some(Listing::from_path(root, route)?);
+        }
         Ok(())
     }
 }
--- src/json/listing.rs
+++ src/json/listing.rs
@@ -1,5 +1,7 @@
 use super::FileInfo;
+use anyhow::Result;
 use serde::Serialize;
+use std::path::Path;
 
 #[derive(Debug, Default, Serialize)]
 pub struct Listing {
@@ -20,5 +22,35 @@ pub struct Sorting {
 impl Listing {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn build(&mut self, root: &Path, route: &Path) -> Result<()> {
+        let path = root.join(route);
+        let items: Vec<FileInfo> = path
+            .read_dir()?
+            .map(|filename| {
+                FileInfo::from_path_without_listing(
+                    root,
+                    &route.join(filename.unwrap().file_name()),
+                )
+                .unwrap()
+            })
+            .collect();
+
+        items.iter().for_each(|fileinfo| match fileinfo.is_dir {
+            true => self.num_dirs += 1,
+            false => self.num_files += 1,
+        });
+
+        self.items = items;
+
+        Ok(())
+    }
+
+    pub fn from_path(root: &Path, route: &Path) -> Result<Self> {
+        let mut listing = Self::default();
+        listing.build(root, route)?;
+
+        Ok(listing)
     }
 }
